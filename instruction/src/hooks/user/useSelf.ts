bc34316839e543ca22f88df2fe886a16b680cf0b
XiangZhuang <xzhuangaa@connect.ust.hk>
2024-08-04 18:25:28 +0800

    Allow signing in and conditionally show instructions #1

---
+++ instruction/src/hooks/user/useSelf.ts
@@ -0,0 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import { QueryKey } from "@/query/queryKey";
+import httpClient from "@/http/httpClient";
+import { UserDto } from "@/type/User";
+import useAuthenticatedQueryFn from "@/hooks/auth/useAuthenticatedQueryFn";
+import useAccessToken from "../auth/useAccessToken";
+
+const useSelf = () => {
+  const { accessToken, clearAccessToken } = useAccessToken();
+  const { data, isLoading, error } = useQuery<Readonly<UserDto> | null>({
+    queryKey: [QueryKey.USER_SELF],
+    queryFn: useAuthenticatedQueryFn(() => httpClient.get("/api/user/self")),
+    gcTime: 3600000,
+    staleTime: 1800000,
+    retry: 0,
+    enabled: !!accessToken,
+  });
+
+  if (error) {
+    clearAccessToken();
+  }
+
+  return {
+    self: data ?? null,
+    isLoading,
+    error,
+  };
+};
+
+export default useSelf;
---
+++ instruction/src/type/User.ts
@@ -0,0 +1,12 @@
+export enum UserRole {
+  ADMIN = "ADMIN",
+  MENTOR = "MENTOR",
+  STUDENT = "STUDENT",
+}
+
+export interface UserDto {
+  username: string;
+  nickname: string;
+  role: UserRole;
+  biography: string;
+}
